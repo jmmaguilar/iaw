c642f3d21cee2f4485468a5ec9d7e28dfa2794ef
Alumno <Alumno@AULA29-15>
2018-10-01 11:16:05 +0200

    Fundamentos

---
+++ Fundamentos01/src/es/cj/fundamentos/datos/Principal.java
@@ -0,0 +1,33 @@
+package es.cj.fundamentos.datos;
+
+public class Principal {
+	public static void main(String[] args) {
+		// Tipos de variables
+			// Tipos enteros
+			byte b; //-128 a + 127
+			short sh; // -32768 a +32767
+			int i; // Recoge 32 bits
+			long l; // Recoge 64 bits
+			
+			char c; // Almacena un caracter
+			char ch = 'a';
+			System.out.println(ch);
+			
+			// Tipos reales
+			float f = 3.1416F; // 32 bits
+			double d; // 64 bits
+			
+			double pi = Math.PI;
+			System.out.println(pi);
+			
+			// Tipo lógico
+			boolean bol = false;
+			
+			// Tipo complejos
+			Byte by;
+			
+			// Tipo cadena
+			String cadena = "Algo";
+			System.out.println(cadena);
+	}
+}
