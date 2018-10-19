82ba40e1bbfe4c38a12b369529bd6c595db71b40
Alumno <Alumno@AULA29-15>
2018-10-19 12:44:54 +0200

    k

--- EjerciciosSemanalesUno/src/es/cj/ejerciciossemanalesdos/ej/EjercicioTres.java
+++ EjerciciosSemanalesUno/src/es/cj/ejerciciossemanalesdos/ej/EjercicioTres.java
@@ -1,10 +1,40 @@
 package es.cj.ejerciciossemanalesdos.ej;
 
+import java.util.Scanner;
+
 public class EjercicioTres {
 
+//	Escriba un programa que lea un número entero por el teclado y muestre
+//	en la pantalla si es un número abundante. Un número abuntante 
+//	si la suma de sus divisores es mayor que él
+	
+	private static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num;
+		// Recogida de datos
+		do {
+			System.out.print("Dime un número: ");
+			num = sc.nextInt();
+		} while (num <= 0);
+		
+		int suma = divisores(num);
+		
+		if (suma > num) {
+			System.out.println("El número " + num + " es abundante, su suma es " + suma);
+		} else {
+			System.out.println("El número " + num + " no es abundante, su suma es " + suma);
+		}
+	}
 
+	private static int divisores(int num) {
+		int sumaDivis = 1;
+		for (int i = 2; i < num ; i++) {
+			if (num % i == 0) {
+				sumaDivis = sumaDivis + i;
+			}
+		}
+		return sumaDivis;
 	}
 
 }
