17664cbc0ecd4aeb6aceb49b243f6bfe0906cec4
Alumno <Alumno@192.168.1.1>
2018-11-06 14:27:04 +0100

    gfd

--- EjerciciosSemanalesTablas/src/es/cj/ejerciciossemanalesuno/ej/EjercicioSemanalProfesor.java
+++ EjerciciosSemanalesTablas/src/es/cj/ejerciciossemanalesuno/ej/EjercicioSemanalProfesor.java
@@ -2,45 +2,95 @@ package es.cj.ejerciciossemanalesuno.ej;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
-public class EjercicioSemanalProfesor{
+public class EjercicioSemanalProfesor {
 
-	// Rellena una tabla de 10 números (valores aleatorios del 1 al 6). Reliza una
+	// Rellena una tabla de 10 números (valores aleatorios del 1 al 6). Realiza una
 	// función que devuelva el número que más se repite.
 
-	private static int tabla[] = new int[10];
-	private static int numRep;
+	private static int numeros[];
+	private static int contadores[];
+	
+	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		inicioTabla();
+		int num = 0;
+		do {
+			System.out.println("Numeros?");
+			num = sc.nextInt();
+		} while (num <= 0);
+		
+		numeros = new int[num];
+		
+		int max = 0;
+		do {
+			System.out.println("Valor máximo?");
+			max = sc.nextInt();
+		} while (max <= 1);
+		
+		contadores = new int[max + 1];
+		
+		rellenarNumeros(max);
+		
+		mostrarNumeros();
+		
+		calcularContadores();
+		
+		mostrarContadores();
+		
+		System.out.println("El que mas se repite es: " + mostrarMasRepetido());
 
-		mostrarTabla();
-
-		numeroRepetido();
-
-		System.out.println("El número que más se repite es: " + numRep);
+		System.out.println("Numeros Repetidos: " + numerosRepetidos());
+		
+		sc.close();
 	}
 
-	private static void numeroRepetido() {
-		int mayor = 0, posicion = 0;
-		Arrays.sort(tabla);
-		for (int i = 0; i < tabla.length; i++) {
-			
+	private static String numerosRepetidos() {
+		String numerosRepes = "";
+		
+		for (int i = 0; i < contadores.length; i++) {
+			if (contadores[i]!=0) {
+				numerosRepes += i + "(" + contadores[i] + ") ";
+			}
+		}
+		return numerosRepes;
+	}
+
+	private static void mostrarContadores() {
+		System.out.println(Arrays.toString(contadores));
+		
+	}
+
+	private static void mostrarNumeros() {
+		System.out.println(Arrays.toString(numeros));
+		
+	}
+
+	private static int mostrarMasRepetido() {
+		int mayor = 0, pos = 0;
+		for (int i = 0; i < contadores.length; i++) {
+			if (contadores[i] > mayor) {
+				mayor = contadores[i];
+				pos = i;
+			}
 		}
 		
-
+		return pos;
 	}
 
-	private static void mostrarTabla() {
-		System.out.println(Arrays.toString(tabla));
-
-	}
-
-	private static void inicioTabla() {
-		for (int i = 0; i < tabla.length; i++) {
-			tabla[i] = new Random().nextInt(5) + 1;
+	private static void calcularContadores() {
+		for (int i = 0; i < numeros.length; i++) {
+			contadores[numeros[i]]++;
 		}
+		
+	}
 
+	private static void rellenarNumeros(int max) {
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = new Random().nextInt(max) + 1;
+		}
+		
 	}
 
 }
